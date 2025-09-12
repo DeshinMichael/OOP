9365829adcbc39f7d65b1f5a8417241815e9b10e
Deshin Mikhail <m.deshin@g.nsu.ru>
2025-09-12 11:32:40 +0700

    created new class ArrayPrinter

---
+++ Task_1_1_1/src/main/java/ArrayPrinter.java
@@ -0,0 +1,16 @@
+public class ArrayPrinter {
+    private ArrayPrinter() {}
+
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            System.err.println("Ошибка: входной массив равен null");
+            return;
+        }
+
+        System.out.print("[");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + (i < arr.length - 1 ? ", " : ""));
+        }
+        System.out.println("]");
+    }
+}
--- Task_1_1_1/src/main/java/Main.java
+++ Task_1_1_1/src/main/java/Main.java
@@ -2,14 +2,6 @@ public class Main {
     public static void main(String[] args) {
         int[] arr = {9, 4, 3, 8, 10, 2, 5};
         HeapSort.heapSort(arr);
-        printArray(arr);
-    }
-
-    private static void printArray(int[] arr) {
-        System.out.print("[");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + (i < arr.length - 1 ? ", " : ""));
-        }
-        System.out.println("]");
+        ArrayPrinter.printArray(arr);
     }
 }
