c0d281bb28ad74bc25171cbd6aa1acf83d7a23f7
Deshin Mikhail <m.deshin@g.nsu.ru>
2025-09-12 11:33:14 +0700

    wrote tests for ArrayPrinter

---
+++ Task_1_1_1/src/test/java/ArrayPrinterTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ArrayPrinterTest {
+
+    @Test
+    public void testPrintArrayWithRegularArray() {
+        int[] arr = {1, 2, 3, 4, 5};
+        assertDoesNotThrow(() -> ArrayPrinter.printArray(arr));
+    }
+
+    @Test
+    public void testPrintArrayWithEmptyArray() {
+        int[] arr = {};
+        assertDoesNotThrow(() -> ArrayPrinter.printArray(arr));
+    }
+
+    @Test
+    public void testPrintArrayWithNullArray() {
+        assertDoesNotThrow(() -> ArrayPrinter.printArray(null));
+    }
+}
