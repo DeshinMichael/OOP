f44daa181880517b637c1559ebc116534786f7cf
Deshin Mikhail <m.deshin@g.nsu.ru>
2025-11-07 23:58:29 +0700

    did task-1-2-2

---
+++ Task_1_2_2/src/main/java/hash_table/core/Entry.java
@@ -0,0 +1,52 @@
+package hash_table.core;
+
+import java.util.Objects;
+
+public class Entry<K, V> {
+    private final K key;
+    private V value;
+    private Entry<K, V> next;
+
+    public Entry(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public Entry<K, V> getNext() {
+        return next;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    public void setNext(Entry<K, V> next) {
+        this.next = next;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Entry<?, ?> entry = (Entry<?, ?>) o;
+        return Objects.equals(key, entry.key) && Objects.equals(value, entry.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(key, value);
+    }
+
+    @Override
+    public String toString() {
+        return key + "=" + value;
+    }
+}
---
+++ Task_1_2_2/src/main/java/hash_table/iterator/HashTableIterator.java
@@ -0,0 +1,61 @@
+package hash_table.iterator;
+
+import hash_table.HashTable;
+import hash_table.core.Entry;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class HashTableIterator<K, V> implements Iterator<Entry<K, V>> {
+    private final Entry<K, V>[] buckets;
+    private final int capacity;
+    private final int expectedModCount;
+    private final HashTable<K, V> table;
+    private int bucketIndex = 0;
+    private Entry<K, V> current = null;
+
+    public HashTableIterator(HashTable<K, V> table, Entry<K, V>[] buckets, int capacity, int modCount) {
+        this.table = table;
+        this.buckets = table.getBuckets();
+        this.capacity = capacity;
+        this.expectedModCount = table.getModCount();
+        this.current = findNextEntry();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return current != null;
+    }
+
+    @Override
+    public Entry<K, V> next() {
+        if (expectedModCount != table.getModCount()) {
+            throw new ConcurrentModificationException();
+        }
+
+        if (current == null) {
+            throw new NoSuchElementException("No more elements in iterator");
+        }
+
+        Entry<K, V> result = current;
+
+        if (current.getNext() != null) {
+            current = current.getNext();
+        } else {
+            bucketIndex++;
+            current = findNextEntry();
+        }
+
+        return result;
+    }
+
+    private Entry<K, V> findNextEntry() {
+        while (bucketIndex < capacity) {
+            if (buckets[bucketIndex] != null) {
+                return buckets[bucketIndex];
+            }
+            bucketIndex++;
+        }
+        return null;
+    }
+}
